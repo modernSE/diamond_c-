a76d7324bb5e9ed11e62e60ca37302dc0fea233d
Tanja.Binder <tanja.binder@yellowmap.de>
2017-06-23 09:08:04 +0200

    initial commit

---
+++ Diamond.Test/DiamondTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Diamond.Test
+{
+    [TestClass]
+    public class DiamondTest
+    {
+        [TestMethod]
+        public void Diamond3_shouldPrint()
+        {
+            var expected = new StringBuilder();
+            expected.Append(" *\n");
+            expected.Append("***\n");
+            expected.Append(" *\n");
+
+            Assert.AreEqual(expected.ToString(), new Diamond().Print(3));
+        }
+
+        [TestMethod]
+        public void Diamond5_shouldPrint()
+        {
+            var expected = new StringBuilder();
+            expected.Append("  *\n");
+            expected.Append(" ***\n");
+            expected.Append("*****\n");
+            expected.Append(" ***\n");
+            expected.Append("  *\n");
+
+            Assert.AreEqual(expected.ToString(), new Diamond().Print(5));
+        }
+
+        [TestMethod]
+        public void Diamond0_shouldReturnNull() 
+        {
+	        Assert.AreEqual(null, new Diamond().Print(0));
+        }
+
+        [TestMethod]
+        public void DiamondNegative_shouldReturnNull()
+        {
+            Assert.AreEqual(null, new Diamond().Print(-1));
+        }
+
+        [TestMethod]
+        public void DiamondEven_shouldReturnNull()
+        {
+            Assert.AreEqual(null, new Diamond().Print(10));
+        }
+
+    }
+}
---
+++ Diamond/Diamond.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond
+{
+    public class Diamond
+    {
+        public String Print(int n)
+        {
+            if (n <= 0 || n % 2 == 0)
+            {
+                return null;
+            }
+            var builder = new StringBuilder(new String(Make(n, n)));
+            for (int i = n - 2; i > 0; i -= 2)
+            {
+                char[] chars = Make(n, i);
+                builder.Insert(0, chars);
+                builder.Append(chars);
+            }
+            return builder.ToString();
+        }
+
+        private char[] Make(int i, int j)
+        {
+            int amount = ((i - j) / 2);
+            var chars = new char[amount + j + 1];
+            if (amount > 0)
+            {
+                Fill(chars, 0, amount, ' ');
+            }
+            Fill(chars, amount, amount + j, '*');
+            chars[chars.Length - 1] = '\n';
+            return chars;
+        }
+
+        private void Fill(char[] sourceArray, int indexFrom, int indexTo, char symbol)
+        {
+            for (var a = indexFrom; a < indexTo; a++)
+            {
+                sourceArray[a] = symbol;
+            }
+        }
+    }
+}
